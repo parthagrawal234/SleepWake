53714efc9b2cc06faf9e9f205bf6bab82ee0618f
Parth Agrawal <personal.parth.agr.234@outlook.com>
2026-05-29 20:01:44 +0530

    first commit

--- agent/src/core/main.cpp
+++ agent/src/core/main.cpp
@@ -1,1 +1,18 @@
-#include ""
+#include "../process_tracker/ProcessTracker.hpp"
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+int main(){
+    ProcessTracker tracker;
+    while (true) {
+        std::cout << tracker.getProcessName()
+        << " | "
+        << tracker.getWindowTitle()
+        << std::endl;
+        std::this_thread::sleep_for(
+            std::chrono::seconds(1)
+        );
+    }
+    return 0;
+}
